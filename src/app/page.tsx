32c1be454c0f37853f43239a315c62b30f46df13
benjaminneoh <benjaminneoh6343@gmail.com>
2024-11-14 15:00:06 +1100

    install plugin for plate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,16 @@
 import { Plate, usePlateEditor } from '@udecode/plate-common/react';
 import { Editor, EditorContainer } from '../components/plate-ui/editor';
 
+import {
+  BoldPlugin,
+  ItalicPlugin,
+  UnderlinePlugin,
+  CodePlugin,
+} from '@udecode/plate-basic-marks/react';
+import { HeadingPlugin } from '@udecode/plate-heading/react';
+import { BlockquotePlugin } from '@udecode/plate-block-quote/react';
+import { CodeBlockPlugin } from '@udecode/plate-code-block/react';
+
 const value = [
   {
     type: 'p',
@@ -16,6 +26,15 @@ const value = [
 export default function Home() {
   const editor = usePlateEditor({
     value,
+    plugins: [
+      HeadingPlugin,
+      BlockquotePlugin,
+      CodeBlockPlugin,
+      BoldPlugin,
+      ItalicPlugin,
+      UnderlinePlugin,
+      CodePlugin
+    ],
   });
 
   return (
