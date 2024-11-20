5a6b52a80a0c02ba0ec34d7e5d4c32a5bf4f5af0
benjaminneoh <benjaminneoh6343@gmail.com>
2024-11-20 16:09:33 +1100

    test with slate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,48 @@
+'use client'
+// Import React dependencies.
+import React, { useState } from 'react'
+// Import the Slate editor factory.
+import { createEditor, Descendant } from 'slate'
+
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
+
+const initialValue: Descendant[] = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.' }],
+  },
+]
+
 export default function Home() {
 
+  const [editor] = useState(() => withReact(createEditor()))
+  
+  // Define a React component renderer for our code blocks.
+  const CodeElement = (props : any) => {
+    return (
+      <pre {...props.attributes}>
+        <code>{props.children}</code>
+      </pre>
+    )
+  }
+
+  const DefaultElement = (props : any) => {
+    return <p {...props.attributes}>{props.children}</p>
+  }
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  );
+    <Slate editor={editor} initialValue={initialValue}>
+      <Editable
+        onKeyDown={event => {
+          if (event.key === '&') {
+            // Prevent the ampersand character from being inserted.
+            event.preventDefault()
+            // Execute the `insertText` method when the event occurs.
+            editor.insertText('and')
+          }
+        }}
+      />
+    </Slate>
+  )
 }
